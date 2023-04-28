a659ae843e48b904620838dfbca5b537a960c718
Blaz-NanoTeK <61864579+Blaz-NanoTeK@users.noreply.github.com>
2023-04-28 19:53:48 +0200

    Secuuu

--- index.js
+++ index.js
@@ -1,57 +1,87 @@
 const fs = require('fs');
+const keep_alive = require('./keep_alive.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const Discord = require('discord.js');
-const client = new Discord.Client({
-  intents: ['GUILDS', 'GUILD_MESSAGES'],
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+  ],
 });
+
 const token = process.env['DISCORDBOT'];
+const logFile = 'log.txt';
 
 client.once('ready', () => {
-    console.log('Ready!');
-    console.log(client.user.username);
+  console.log('Ready!');
+  console.log(client.user.username);
 });
 
 client.on('ready', () => {
-    console.log('Salut mon chat');
-    postDailyMotivation();
+  console.log('Salut mon chat');
+  postDailyMotivation();
 
-    setInterval(postDailyMotivation, 24 * 60 * 60 * 1000); // 24 heures
+  setInterval(postDailyMotivation, 24 * 30 * 60 * 1000); // 24 heures
 });
 
 async function postDailyMotivation() {
-    console.log('Salut ma biche');
-    const currentDate = new Date();
-    const dayOfWeek = currentDate.getDay();
+  console.log('Salut ma biche');
+  const currentDate = new Date();
+  const dayOfWeek = currentDate.getDay();
 
-    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-        const motivations = fs.readFileSync('motivations.txt', 'utf8').split('===');
-        const motivationCounterFile = 'motivation_counter.txt';
+  if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+    const motivationsFile = 'motivations.txt';
+    const motivationCounterFile = 'motivation_counter.txt';
 
-        let counter;
-        if (fs.existsSync(motivationCounterFile)) {
-            counter = parseInt(fs.readFileSync(motivationCounterFile, 'utf8'));
-        } else {
-            counter = 0;
-        }
-
-        const motivation = motivations[counter % motivations.length].trim();
-        const guilds = Array.from(client.guilds.cache.values());
-
-        for (const guild of guilds) {
-            const channelId = '1092781430251204628'; // Remplacez YOUR_CHANNEL_ID par l'ID du canal
-            const channel = client.channels.cache.get(channelId);
-
-            if (channel) {
-                console.log(`Found channel: ${channel.name}`);
-                await channel.send(motivation);
-            } else {
-                console.log('Channel not found');
-            }
-        }
-
-        counter++;
-        fs.writeFileSync(motivationCounterFile, counter.toString(), 'utf8');
+    if (!fs.existsSync(motivationsFile)) {
+      console.error(`Le fichier ${motivationsFile} n'existe pas.`);
+      return;
     }
+
+    const motivations = fs.readFileSync(motivationsFile, 'utf8').split('===');
+
+    let counter;
+    if (fs.existsSync(motivationCounterFile)) {
+      counter = parseInt(fs.readFileSync(motivationCounterFile, 'utf8'));
+    } else {
+      counter = 0;
+    }
+
+    // Vérifier si le compteur est supérieur à la taille de la liste des motivations
+    if (counter >= motivations.length) {
+      counter = 0;
+    }
+
+    const motivation = motivations[counter].trim();
+    const guilds = Array.from(client.guilds.cache.values());
+
+    for (const guild of guilds) {
+      const channelId = '1092781430251204628'; // Remplacez YOUR_CHANNEL_ID par l'ID du canal
+      const channel = client.channels.cache.get(channelId);
+
+      if (channel) {
+        console.log(`Found channel: ${channel.name}`);
+        await channel.send(motivation);
+        logPublication(motivation, channel);
+      } else {
+        console.log('Channel not found');
+      }
+    }
+
+    counter++;
+    fs.writeFileSync(motivationCounterFile, counter.toString(), 'utf8');
+  }
+}
+
+function logPublication(motivation, channel) {
+  const currentDate = new Date();
+  const logMessage = `${currentDate.toLocaleString()} : Publié "${motivation}" sur le canal ${channel.name} (${channel.id})\n`;
+
+  fs.appendFile(logFile, logMessage, (err) => {
+    if (err) {
+      console.error('Erreur lors de l\'écriture dans le fichier de log :', err);
+    }
+  });
 }
 
 client.login(token);
